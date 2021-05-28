ed26d7524fc67aa3e267be6b560860fe25bc6a86
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-05-28 14:10:39 +0200

    activated heroku url

--- client/src/shared/api/UserAPI.ts
+++ client/src/shared/api/UserAPI.ts
@@ -4,7 +4,7 @@ const productionAPI = 'https://thawing-mountain-25749.herokuapp.com/'
 const developmentAPI = 'http://localhost:3001';
 
 const LenderAPI = Axios.create({
-    baseURL: developmentAPI,
+    baseURL: productionAPI,
     headers: { 'Content-Type': 'application/json' }
 });
 
